d0296c9f0a389df0961da7f0030aac04bb256c4c
Mateen Nawaz <mateenmirani125@gmail.com>
2024-05-29 10:22:19 +0500

    basic fullstack backend

--- basic_backend/index.js
+++ basic_backend/index.js
@@ -1,8 +1,10 @@
-console.log("Hello world!");
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT | 5000;
+console.log(port);
 
 app.get("/", (req, res) => {
   res.send("Hello Mateen");
---
+++ basic_fullStack/backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT | 5000;
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hello World!</h2>");
+});
+
+app.get("/products", (req, res) => {
+  const PRODUCTS = [
+    {
+      id: 1,
+      title: "First Product",
+      description: "This is first product",
+    },
+    {
+      id: 2,
+      title: "Second Product",
+      description: "This is second product",
+    },
+    {
+      id: 3,
+      title: "Third Product",
+      description: "This is third product",
+    },
+  ];
+
+  res.json(PRODUCTS);
+});
+
+app.listen(port, () => {
+  console.log(`This app is listening on the ${port}.`);
+});
