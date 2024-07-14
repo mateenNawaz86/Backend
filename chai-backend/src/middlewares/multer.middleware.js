ae6e9b171193fdd00c26fc37df74c8fe9dfbe52a
Mateen Nawaz <mateenmirani125@gmail.com>
2024-07-14 17:50:00 +0500

    file upload to cloudinary setup

---
+++ chai-backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9); // this is used to generate random string and attach with each uploaded file
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname); // here we can only get the orignal name of file that's not a good practice but we change this later
+  },
+});
+
+// export const upload = multer({ storage: storage });
+export const upload = multer({ storage }); // es6 version
---
+++ chai-backend/src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; // file system of nodejs
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SCREAT,
+});
+
+// method for handling file upload on cloudinary
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    // IF file has been uploaded successfully
+    console.log(`File upload on cloudinary successfully`, response.url);
+    return response;
+  } catch (error) {
+    // IF uploaded file operation failed then remove the locally saved file
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
