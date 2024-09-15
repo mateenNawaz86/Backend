e5acb41c686fdbbf34e592df1bfa60f145ba7106
Mateen Nawaz <mateenmirani125@gmail.com>
2024-09-15 13:18:28 -0400

    calculate subscribers & subscribed to using aggregation pipeline

--- chai-backend/src/controllers/user.controller.js
+++ chai-backend/src/controllers/user.controller.js
@@ -266,7 +266,7 @@ export const changeCurrentPassword = asyncHandler(async (req, res) => {
 export const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req?.user, "current user fetching successfly");
+    .json(new ApiResponse(200, req?.user, "current user fetching successfly"));
 });
 
 
@@ -290,7 +290,6 @@ export const updateAccountDetails = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, user, "Account detail updated successfully!"))
-
 }
 )
 
@@ -298,14 +297,11 @@ export const updateAccountDetails = asyncHandler(async (req, res) => {
 export const updateUserAvatar = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.file?.path;
 
-
   if (avatarLocalPath) {
     throw new ApiError(400, "Avatar file is missing!")
   }
 
-
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-
 
   if (!avatar.url) {
     throw new ApiError(400, "Error while uploading an avatar image!")
@@ -354,6 +350,95 @@ export const updateUserCoverImg = asyncHandler(async (req, res) => {
 
 
   return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image update successfully!"))
+})
+
+
+
+export const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+  // get username from url
+  const { username } = req.params
+
+  // IF username doesn't exist
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing!")
+  }
+
+  // aggregation
+  const channel = await User.aggregate([
+
+    // match pipeline
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+
+    // count the subscriber pipeline
+    {
+      $lookup: {
+        from: "subscriptions",       // The collection to join (assumes collection name is 'subscriptions')
+        localField: "_id",          // Field from the `User` collection to match
+        foreignField: "channel",   // Field from the `channels` collection to match
+        as: "subscribers"          // Output array field for the joined data
+      }
+    },
+
+    // count the subsribed to pipeline
+    {
+      $lookup: {
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribers"
+      }
+    },
+
+
+    // calculate the subscribers & subscribed to count add fields pipeline
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size:"$subscribers"
+        },
+        isSubscribed: {
+          $cond:{
+            if:{$in: [req.user?._id, "$subscribers.subscriber"]},
+            then:true,
+            else:false
+          }
+        }
+      }
+    },
+
+
+    // return response pipeline
+    {
+      $project: {
+        fullName:1,
+        username:1,
+        email:1,
+        avatar:1,
+        coverImage:1,
+        isSubscribed:1,
+        subscribersCount:1,
+        channelsSubscribedToCount:1,
+      }
+    }
+  ])
+
+  // if channel not found 
+  if(!channel?.length) {
+    throw new ApiError(404, "Channel does not exists")
+  }
+
+  // return response
+  return res
   .status(200)
-  .json(new ApiResponse(200, user, "Cover Image update successfully!"))
+  .json(new  ApiResponse(200, channel[0], "User channel fetched successfully!"))
 })
